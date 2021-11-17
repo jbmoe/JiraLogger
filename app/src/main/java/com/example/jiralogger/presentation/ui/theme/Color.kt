a74385f8147b37121b63e0dd88f67e302cf26051
Jeppe <jeppe.bach248@hotmail.com>
2021-11-17 15:30:18 +0100

    Added material theme 3 colors for outlined text field

--- app/src/main/java/com/example/jiralogger/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/jiralogger/presentation/ui/theme/Color.kt
@@ -3,17 +3,44 @@ package com.example.jiralogger.presentation.ui.theme
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jiralogger.presentation.components.Text
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF505050)
+val ColorPrimary = Color(0xFF08FF04)
+val TextWhite = Color(0xFFEEEEEE)
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
+
+val primaryColor = Color(0xffffde03)
+val primaryLightColor = Color(0xffffff54)
+val primaryDarkColor = Color(0xffc7ad00)
+val secondaryColor = Color(0xff0336ff)
+val secondaryLightColor = Color(0xff6f63ff)
+val secondaryDarkColor = Color(0xff0008ca)
+val primaryTextColor = Color(0xFF000000)
+val secondaryTextColor = Color(0xffffffff)
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+
 
 @Composable
 fun outlinedTextFieldColors(
-    textColor: Color = MaterialTheme.colorScheme.onBackground,
+    textColor: Color = LocalContentColor.current.copy(LocalContentAlpha.current),
     disabledTextColor: Color = textColor.copy(ContentAlpha.disabled),
     backgroundColor: Color = Color.Transparent,
     cursorColor: Color = MaterialTheme.colorScheme.primary,
@@ -69,62 +96,77 @@ fun Preview() {
         Column {
             Text(
                 text = "displayLarge",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.displayLarge
             )
             Text(
                 text = "displayMedium",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.displayMedium
             )
             Text(
                 text = "displaySmall",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.displaySmall
             )
             Text(
                 text = "headlineLarge",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.headlineLarge
             )
             Text(
                 text = "headlineMedium",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.headlineMedium
             )
             Text(
                 text = "headlineSmall",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.headlineSmall
             )
             Text(
                 text = "titleLarge",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.titleLarge
             )
             Text(
                 text = "titleMedium",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
                 text = "titleSmall",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.titleSmall
             )
             Text(
                 text = "bodyLarge",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.bodyLarge
             )
             Text(
                 text = "bodyMedium",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.bodyMedium
             )
             Text(
                 text = "bodySmall",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.bodySmall
             )
             Text(
                 text = "labelLarge",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.labelLarge
             )
             Text(
                 text = "labelMedium",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.labelMedium
             )
             Text(
                 text = "labelSmall",
+                color = MaterialTheme.colorScheme.onBackground,
                 style = MaterialTheme.typography.labelSmall
             )
         }
