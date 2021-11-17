b88824c9cebb62c9b00b0e70d409a36221a52131
Jeppe <jeppe.bach248@hotmail.com>
2021-11-17 15:30:18 +0100

    Added material theme 3 colors for outlined text field

--- app/src/main/java/com/example/jiralogger/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/jiralogger/presentation/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package com.example.jiralogger.presentation.ui.theme
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,6 +36,56 @@ val Red300 = Color(0xffea6d7e)
 val Red700 = Color(0xffdd0d3c)
 val Red800 = Color(0xffd00036)
 val Red900 = Color(0xffc20029)
+
+
+@Composable
+fun outlinedTextFieldColors(
+    textColor: Color = LocalContentColor.current.copy(LocalContentAlpha.current),
+    disabledTextColor: Color = textColor.copy(ContentAlpha.disabled),
+    backgroundColor: Color = Color.Transparent,
+    cursorColor: Color = MaterialTheme.colorScheme.primary,
+    errorCursorColor: Color = MaterialTheme.colorScheme.error,
+    focusedBorderColor: Color = MaterialTheme.colorScheme.primary.copy(alpha = ContentAlpha.high),
+    unfocusedBorderColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = ContentAlpha.disabled),
+    disabledBorderColor: Color = unfocusedBorderColor.copy(alpha = ContentAlpha.disabled),
+    errorBorderColor: Color = MaterialTheme.colorScheme.error,
+    leadingIconColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = TextFieldDefaults.IconOpacity),
+    disabledLeadingIconColor: Color = leadingIconColor.copy(alpha = ContentAlpha.disabled),
+    errorLeadingIconColor: Color = leadingIconColor,
+    trailingIconColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = TextFieldDefaults.IconOpacity),
+    disabledTrailingIconColor: Color = trailingIconColor.copy(alpha = ContentAlpha.disabled),
+    errorTrailingIconColor: Color = MaterialTheme.colorScheme.error,
+    focusedLabelColor: Color = MaterialTheme.colorScheme.primary.copy(alpha = ContentAlpha.high),
+    unfocusedLabelColor: Color = MaterialTheme.colorScheme.onSurface.copy(ContentAlpha.medium),
+    disabledLabelColor: Color = unfocusedLabelColor.copy(ContentAlpha.disabled),
+    errorLabelColor: Color = MaterialTheme.colorScheme.error,
+    placeholderColor: Color = MaterialTheme.colorScheme.onSurface.copy(ContentAlpha.medium),
+    disabledPlaceholderColor: Color = placeholderColor.copy(ContentAlpha.disabled)
+): TextFieldColors =
+    MaterialThemeOutlinedTextFieldColors(
+        textColor = textColor,
+        disabledTextColor = disabledTextColor,
+        cursorColor = cursorColor,
+        errorCursorColor = errorCursorColor,
+        focusedIndicatorColor = focusedBorderColor,
+        unfocusedIndicatorColor = unfocusedBorderColor,
+        errorIndicatorColor = errorBorderColor,
+        disabledIndicatorColor = disabledBorderColor,
+        leadingIconColor = leadingIconColor,
+        disabledLeadingIconColor = disabledLeadingIconColor,
+        errorLeadingIconColor = errorLeadingIconColor,
+        trailingIconColor = trailingIconColor,
+        disabledTrailingIconColor = disabledTrailingIconColor,
+        errorTrailingIconColor = errorTrailingIconColor,
+        backgroundColor = backgroundColor,
+        focusedLabelColor = focusedLabelColor,
+        unfocusedLabelColor = unfocusedLabelColor,
+        disabledLabelColor = disabledLabelColor,
+        errorLabelColor = errorLabelColor,
+        placeholderColor = placeholderColor,
+        disabledPlaceholderColor = disabledPlaceholderColor
+    )
+
 
 @Preview(name = "Light mode", uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
