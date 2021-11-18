9c1da289a2d104e866de7b36e298955c40c140ee
Jeppe <jeppe.bach248@hotmail.com>
2021-11-18 17:15:25 +0100

    tabs are now initially shown

--- app/src/main/java/com/example/jiralogger/presentation/issue_list/IssueListState.kt
+++ app/src/main/java/com/example/jiralogger/presentation/issue_list/IssueListState.kt
@@ -2,11 +2,14 @@ package com.example.jiralogger.presentation.issue_list
 
 import com.example.jiralogger.domain.model.Issue
 import com.example.jiralogger.domain.util.IssueFilter
+import com.example.jiralogger.presentation.util.UiListState
 
 data class IssueListState(
-    val isLoading: Boolean = false,
-    val items: List<Issue> = emptyList(),
-    val error: String = "",
+    override val isLoading: Boolean = false,
+    override val items: List<Issue> = emptyList(),
+    override val itemMap: Map<Any, List<Any>> = emptyMap(),
+    override val error: String = "",
+    val filterIsVisible: Boolean = true,
     val searchIsVisible: Boolean = false,
     val issueFilter: IssueFilter = IssueFilter.Assigned
-)
+): UiListState
