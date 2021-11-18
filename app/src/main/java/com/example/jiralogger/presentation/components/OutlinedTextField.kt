90460b929a7e6c38f1726ca924d4239620d7405d
Jeppe <jeppe.bach248@hotmail.com>
2021-11-18 22:18:24 +0100

    added error ui

--- app/src/main/java/com/example/jiralogger/presentation/components/OutlinedTextField.kt
+++ app/src/main/java/com/example/jiralogger/presentation/components/OutlinedTextField.kt
@@ -1,16 +1,29 @@
 package com.example.jiralogger.presentation.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.webkit.WebChromeClient
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jiralogger.presentation.ui.theme.JiraLoggerTheme
 import com.example.jiralogger.presentation.ui.theme.outlinedTextFieldColors
 import com.example.jiralogger.presentation.util.InputFieldState
 
@@ -21,6 +34,7 @@ fun OutlinedTextField(
     modifier: Modifier = Modifier,
     value: String,
     isError: Boolean = false,
+    errorText: String = "",
     labelText: String = "",
     placeholderText: String = "",
     visualTransformation: VisualTransformation = VisualTransformation.None,
@@ -34,25 +48,30 @@ fun OutlinedTextField(
     val onBackground = MaterialTheme.colorScheme.onBackground
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    OutlinedTextField(
-        modifier = modifier,
-        value = value,
-        onValueChange = onValueChange,
-        isError = isError,
-        visualTransformation = visualTransformation,
-        textStyle = TextStyle(onBackground),
-        label = textOrNull(labelText),
-        singleLine = singleLine,
-        maxLines = maxLines,
-        readOnly = readOnly,
-        placeholder = textOrNull(placeholderText),
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        keyboardActions = KeyboardActions(onDone = {
-            keyboardController?.hide()
-        }),
-        colors = outlinedTextFieldColors()
-    )
+    Column {
+        OutlinedTextField(
+            modifier = modifier,
+            value = value,
+            onValueChange = onValueChange,
+            isError = isError,
+            visualTransformation = visualTransformation,
+            textStyle = TextStyle(onBackground),
+            label = textOrNull(labelText),
+            singleLine = singleLine,
+            maxLines = maxLines,
+            readOnly = readOnly,
+            placeholder = textOrNull(placeholderText),
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIconOrError(trailingIcon = trailingIcon, isError = isError),
+            keyboardActions = KeyboardActions(onDone = {
+                keyboardController?.hide()
+            }),
+            colors = outlinedTextFieldColors()
+        )
+        if (isError and errorText.isNotEmpty()) {
+            Text(text = errorText, color = MaterialTheme.colorScheme.error)
+        }
+    }
 }
 
 @ExperimentalComposeUiApi
@@ -74,6 +93,8 @@ fun OutlinedTextField(
         value = inputState.value,
         labelText = inputState.label,
         placeholderText = inputState.placeholder,
+        isError = inputState.isError,
+        errorText = inputState.error,
         visualTransformation = visualTransformation,
         singleLine = singleLine,
         maxLines = maxLines,
@@ -91,5 +112,60 @@ private fun textOrNull(text: String): @Composable (() -> Unit)? {
         { Text(text = text, color = MaterialTheme.colorScheme.onBackground) }
     } else {
         null
+    }
+}
+
+@Composable
+fun trailingIconOrError(
+    trailingIcon: @Composable (() -> Unit)?,
+    isError: Boolean
+): @Composable (() -> Unit)? {
+    return if (isError) {
+        { Icon(Icons.Default.Warning, "error", tint = MaterialTheme.colorScheme.error) }
+    } else {
+        trailingIcon
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+@Preview(name = "Light Mode", uiMode = UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(name = "Dark Mode", uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+fun PreviewOTF() {
+    val username = InputFieldState(
+        value = "",
+        label = "Username",
+        placeholder = "Your username",
+        error = "Wrong username",
+        isError = false,
+    )
+    val password = InputFieldState(
+        value = "",
+        label = "Password",
+        placeholder = "Your password",
+        error = "Wrong password",
+        isError = true,
+    )
+    JiraLoggerTheme {
+        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column {
+                OutlinedTextField(
+                    value = username.value,
+                    labelText = username.label,
+                    placeholderText = username.placeholder,
+                    isError = username.isError,
+                    errorText = username.error,
+                    onValueChange = {}
+                )
+                OutlinedTextField(
+                    value = password.value,
+                    labelText = password.label,
+                    placeholderText = password.placeholder,
+                    isError = password.isError,
+                    errorText = password.error,
+                    onValueChange = {}
+                )
+            }
+        }
     }
 }
