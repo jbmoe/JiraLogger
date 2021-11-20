312c29573b603a15ab8bcfb0d79a26feb3afce3e
Jeppe <jeppe.bach248@hotmail.com>
2021-11-20 14:38:13 +0100

    dagger hilt didnt like updated dependency

--- app/src/main/java/com/example/jiralogger/presentation/work_log_list/WorkLogListState.kt
+++ app/src/main/java/com/example/jiralogger/presentation/work_log_list/WorkLogListState.kt
@@ -1,12 +1,11 @@
 package com.example.jiralogger.presentation.work_log_list
 
 import com.example.jiralogger.domain.model.WorkLog
-import com.example.jiralogger.domain.util.OrderType
 import com.example.jiralogger.domain.util.WorkLogGroupBy
 
 data class WorkLogListState(
     val itemMap: Map<String, List<WorkLog>> = emptyMap(),
     val isLoading: Boolean = false,
     val error: String = "",
-    val groupBy: WorkLogGroupBy = WorkLogGroupBy.Date(OrderType.Descending)
+    val groupBy: WorkLogGroupBy = WorkLogGroupBy.Date
 )
