47af37d6634939f59c023443f1626960120e2bf4
Jeppe <jeppe.bach248@hotmail.com>
2021-11-20 14:19:35 +0100

    beginning groupby functionality

--- app/src/main/java/com/example/jiralogger/presentation/util/preview_paramater/WorkLogListPreviewParameterProvider.kt
+++ app/src/main/java/com/example/jiralogger/presentation/util/preview_paramater/WorkLogListPreviewParameterProvider.kt
@@ -3,8 +3,12 @@ package com.example.jiralogger.presentation.util.preview_paramater
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.example.jiralogger.common.test_data.TestData
 import com.example.jiralogger.presentation.work_log_list.WorkLogListState
+import java.text.SimpleDateFormat
 
 class WorkLogListPreviewParameterProvider : PreviewParameterProvider<WorkLogListState> {
     override val values: Sequence<WorkLogListState>
-        get() = sequenceOf(WorkLogListState(itemMap = TestData.WORK_LOG_TEST_DATA.groupBy { it.dateWorked }))
+        get() = sequenceOf(WorkLogListState(itemMap = TestData.WORK_LOG_TEST_DATA.groupBy {
+            val sdf = SimpleDateFormat("E d. MMMM yy")
+            sdf.format(it.dateWorked)
+        }))
 }
