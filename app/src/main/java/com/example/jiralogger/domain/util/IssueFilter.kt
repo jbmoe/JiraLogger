021106de96edf0258ff7cc1293320ba9d8ce9b4a
Jeppe Møller <jem@elbek-vejrup.dk>
2021-11-23 20:15:30 +0100

    Refractored

--- app/src/main/java/com/example/jiralogger/domain/util/IssueFilter.kt
+++ app/src/main/java/com/example/jiralogger/domain/util/IssueFilter.kt
@@ -10,7 +10,7 @@ import com.example.jiralogger.common.constant.Filters
 sealed class IssueFilter(val value: String, override val name: String) : HasName {
     object Assigned : IssueFilter(Filters.ASSIGNED_TO_ME, "Assigned")
     object Seen : IssueFilter(Filters.LAST_SEEN, "Seen")
-    object WATCHING : IssueFilter(Filters.WATCHING, "Watching")
+    object Watching : IssueFilter(Filters.WATCHING, "Watching")
     object EV : IssueFilter(Filters.EV, "EV")
     data class SEARCH(val search: String) : IssueFilter(Filters.SEARCH_PREFIX + "$search", "Search")
 }
--- app/src/main/java/com/example/jiralogger/presentation/MainActivity.kt
+++ app/src/main/java/com/example/jiralogger/presentation/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JiraLoggerTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colorScheme.background) {
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
