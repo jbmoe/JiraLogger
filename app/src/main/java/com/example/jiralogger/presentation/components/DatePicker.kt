288732615207d48b64ace8883600d77c9009bfc3
Jeppe Møller <jem@elbek-vejrup.dk>
2021-11-23 18:05:43 +0100

    Better DatePicker.kt, click wherever expands

--- app/src/main/java/com/example/jiralogger/presentation/components/DatePicker.kt
+++ app/src/main/java/com/example/jiralogger/presentation/components/DatePicker.kt
@@ -3,12 +3,13 @@ package com.example.jiralogger.presentation.components
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -16,12 +17,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.jiralogger.presentation.issue_list.IssueListState
 import com.example.jiralogger.presentation.ui.theme.JiraLoggerTheme
-import com.example.jiralogger.presentation.ui.theme.outlinedTextFieldColors
 import com.example.jiralogger.presentation.util.preview_paramater.IssueListPreviewParameterProvider
 import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.SimpleDateFormat
 import java.util.*
 
+@ExperimentalMaterialApi
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun DatePicker(
@@ -34,33 +35,45 @@ fun DatePicker(
     val sdf = SimpleDateFormat(dateFormat)
     val display = sdf.format(Date(selectedDate))
 
-    OutlinedTextField(
-        modifier = modifier,
-        value = display,
-        labelText = "Date",
-        onValueChange = {
+    var expanded by remember { mutableStateOf(false) }
 
-        },
-        trailingIcon = {
-            IconButton(onClick = {
-                showDatePicker(activity = activity, selectedDate = selectedDate) {
-                    datePicked(it)
-                }
-            }) {
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        OutlinedTextField(
+            modifier = modifier,
+            value = display,
+            labelText = "Date",
+            onValueChange = {
+
+            },
+            trailingIcon = {
                 Icon(
                     imageVector = Icons.Default.DateRange,
                     contentDescription = "Calendar",
                     tint = MaterialTheme.colorScheme.onBackground
                 )
-            }
-        },
-        readOnly = true,
-    )
+            },
+            readOnly = true,
+        )
+    }
+    if (expanded) {
+        showDatePicker(
+            activity = activity,
+            selectedDate = selectedDate,
+            onDismiss = { expanded = false }) {
+            datePicked(it)
+        }
+    }
 }
 
 private fun showDatePicker(
     selectedDate: Long,
     activity: AppCompatActivity,
+    onDismiss: () -> Unit,
     datePicked: (Long) -> Unit
 ) {
     val datePicker = MaterialDatePicker.Builder.datePicker()
@@ -72,9 +85,14 @@ private fun showDatePicker(
         datePicked(it)
     }
 
+    datePicker.addOnDismissListener {
+        onDismiss()
+    }
+
     datePicker.show(activity.supportFragmentManager, datePicker.toString())
 }
 
+@ExperimentalMaterialApi
 @OptIn(ExperimentalAnimationApi::class)
 @Preview(name = "Light mode", uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
