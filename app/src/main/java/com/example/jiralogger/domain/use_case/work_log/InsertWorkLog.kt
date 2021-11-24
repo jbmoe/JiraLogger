6079233d291e796668f7ec96741da53fe7dc1b89
Jeppe Møller <jem@elbek-vejrup.dk>
2021-11-24 20:45:30 +0100

    error if no issue selected

--- app/src/main/java/com/example/jiralogger/domain/use_case/work_log/InsertWorkLog.kt
+++ app/src/main/java/com/example/jiralogger/domain/use_case/work_log/InsertWorkLog.kt
@@ -1,7 +1,6 @@
 package com.example.jiralogger.domain.use_case.work_log
 
 import com.example.jiralogger.domain.model.InvalidLogException
-import com.example.jiralogger.domain.model.LogError
 import com.example.jiralogger.domain.model.WorkLog
 import com.example.jiralogger.domain.repository.DbRepository
 import javax.inject.Inject
@@ -11,9 +10,12 @@ class InsertWorkLog @Inject constructor(
 ) {
     @Throws(InvalidLogException::class)
     suspend operator fun invoke(workLog: WorkLog) {
-        if (workLog.issueId.isBlank()) throw InvalidLogException(LogError.IssueID)
-        if (workLog.dateWorked == 0L) throw InvalidLogException(LogError.Date)
-        if (workLog.timeSpent.isBlank()) throw InvalidLogException(LogError.TimeSpent)
+        if(workLog.issueId.isBlank()) {
+            throw InvalidLogException("You must pick an issue")
+        }
+        if (workLog.timeSpent.isBlank()) {
+            throw InvalidLogException("You must specify amount of time")
+        }
         repository.insertWorkLog(workLog)
     }
 }
